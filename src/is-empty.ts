a4b77191d27a79be46857bff812266b7694471d5
Sébastien Garcia-Romeu <webmaster@nicewebagence.com>
2023-06-14 10:43:51 +0200

    FIX isEmpty not returning value

--- src/is-empty.ts
+++ src/is-empty.ts
@@ -1,5 +1,5 @@
 export function isEmpty(objOrArr: object | any[] | string | null | undefined) {
     if (Array.isArray(objOrArr) || typeof objOrArr === 'string') return objOrArr.length === 0
     else if (typeof objOrArr == 'object' && objOrArr !== null && !(objOrArr instanceof Date)) return Object.keys(objOrArr).length === 0
-    else false
-}
+    else return false
+  }
