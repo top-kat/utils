915c402c322f8a0dfeea2f2e301dd234e94a51b1
Sébastien Garcia-Romeu <webmaster@nicewebagence.com>
2024-02-22 13:51:36 +0100

    documentation improvements

--- src/remove-circular-json-stringify.ts
+++ src/remove-circular-json-stringify.ts
@@ -1,4 +1,6 @@
 
+
+/** Best way to stringify a value! Default indent: 2 */
 export function removeCircularJSONstringify(object, indent = 2) {
     const getCircularReplacer = () => {
         const seen = new WeakSet()
